f0d8453d329a943bd29e8b10d087f69c8a287ecc
Leni-microweather <Leni-microweather@proton.me>
2023-04-22 18:18:24 +0800

    config: 配置别名

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from '@/app.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [],
+    imports: [],
+    controllers: [AppController],
+    providers: [],
 })
 export class AppModule {}
