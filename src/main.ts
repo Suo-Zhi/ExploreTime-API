503ab39645bc058ba0b01744bd29a71bc25a988e
Leni-microweather <Leni-microweather@proton.me>
2023-04-22 23:19:56 +0800

    config: 配置请求前置路径

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useStaticAssets('uploads', { prefix: '/static' }); // 静态资源访问目录
     app.useGlobalInterceptors(new TransformInterceptor()); // 响应数据包裹
+    app.setGlobalPrefix('api'); // 请求前置路径
     await app.listen(config().port);
 }
 bootstrap();
