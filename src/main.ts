2b20c4af46e2ed26ebe90f9cc2a95bfc60c3db01
Leni-microweather <Leni-microweather@proton.me>
2023-04-22 23:27:34 +0800

    config: 配置跨域请求

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import config from '@/config';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 
 async function bootstrap() {
@@ -9,6 +8,7 @@ async function bootstrap() {
     app.useStaticAssets('uploads', { prefix: '/static' }); // 静态资源访问目录
     app.useGlobalInterceptors(new TransformInterceptor()); // 响应数据包裹
     app.setGlobalPrefix('api'); // 请求前置路径
-    await app.listen(config().port);
+    app.enableCors(); // 跨域请求Cors
+    await app.listen(process.env.NODE_PORT);
 }
 bootstrap();
