1830963a431909f241384f8ee97ae46723a39d78
Leni-microweather <Leni-microweather@proton.me>
2023-05-06 05:48:19 +0800

    alter(tree): 调整返回的节点结构 & 获取节点标题前缀

--- src/module/tree/tree-node/tree-node.service.ts
+++ src/module/tree/tree-node/tree-node.service.ts
@@ -39,28 +39,53 @@ export class TreeNodeService {
             });
     }
 
-    getChildNode(nodes: TreeNode[], parentNodeId: number = null, deep: number = 1) {
+    getChildNode(nodes: TreeNode[], parentNodeId: number = null, deep: number = 1, parentPrefix: string = '') {
         // 获取当前层节点
         const currentNodes = nodes.filter((node) => node.parentNodeId === parentNodeId);
         return currentNodes.map((node: any) => {
+            // 获取节点标题前缀
+            const prefix = this.getNodeLevel(deep, node.order, parentPrefix);
+            // 整理块信息
             const { ChunkContent, ...chunk } = node.Node;
             const content = ChunkContent.map((item: any) => {
                 return { ...item.Point, order: item.order };
             });
             return {
                 ...chunk, // 节点对应的块信息
-                nodeId: node.id, // 节点Id
-                order: node.order, // 节点在层中的顺序
                 content, // 块内容
-                deep, // 层级
-                nodes: this.getChildNode(nodes, node.id, deep + 1), // 子节点
+
+                node: {
+                    id: node.id, // 节点Id
+                    treeId: node.treeId, // 所在树Id
+                    parentId: node.parentNodeId, // 父节点Id
+                    order: node.order, // 节点在层中的顺序
+                    children: this.getChildNode(nodes, node.id, deep + 1, prefix), // 子节点
+                },
+
+                level: {
+                    deep, // 节点层级
+                    prefix, // 节点标题前缀
+                },
             };
         });
     }
 
+    getNodeLevel(deep: number, order: number, parentPrefix: string = '') {
+        const firstLevel = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一'];
+        switch (deep) {
+            case 1:
+                return firstLevel[order] + '、';
+            case 2:
+                return order + 1 + '.';
+            case 3:
+                return parentPrefix + (order + 1);
+            default:
+                return parentPrefix + '.' + (order + 1);
+        }
+    }
+
     upsert(id: number, dto: UpsertTreeNodeDTO) {
         dto = plainToClass(UpsertTreeNodeDTO, dto);
-        console.log(dto);
 
         return this.prisma.treeNode.upsert({
             where: { id },
