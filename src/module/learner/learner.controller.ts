4061f576adc5debfa829c144bfddbceb6637d03b
Leni-microweather <Leni-microweather@proton.me>
2023-05-09 03:31:36 +0800

    feat(learner): 获取学习者信息API

--- src/module/learner/learner.controller.ts
+++ src/module/learner/learner.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { LearnerService } from './learner.service';
 
 @Controller('learner')
 export class LearnerController {
-  constructor(private readonly learnerService: LearnerService) {}
+    constructor(private readonly learnerService: LearnerService) {}
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.learnerService.findOne(id);
+    }
 }
--- src/module/learner/learner.service.ts
+++ src/module/learner/learner.service.ts
@@ -4,4 +4,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class LearnerService {
     constructor(private readonly prisma: PrismaService) {}
+
+    async findOne(id: string) {
+        const { password, ...userInfo } = await this.prisma.learner.findUnique({
+            where: { id },
+        });
+        return userInfo;
+    }
 }
