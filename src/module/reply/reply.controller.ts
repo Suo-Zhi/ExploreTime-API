d8c352629f244289d0a3581a1db28d9bad8a7a3e
Leni-microweather <Leni-microweather@proton.me>
2023-05-09 18:58:41 +0800

    feat(reply): 获取根回复API

--- src/module/reply/reply.controller.ts
+++ src/module/reply/reply.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ReplyService } from './reply.service';
 
 @Controller('reply')
 export class ReplyController {
-  constructor(private readonly replyService: ReplyService) {}
+    constructor(private readonly replyService: ReplyService) {}
+
+    @Get('root:feedbackId')
+    getRoot(@Param('feedbackId') feedbackId: number) {
+        return this.replyService.getRoot(+feedbackId);
+    }
 }
--- src/module/reply/reply.service.ts
+++ src/module/reply/reply.service.ts
@@ -4,4 +4,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ReplyService {
     constructor(private readonly prisma: PrismaService) {}
+
+    getRoot(feedbackId: number) {
+        return this.prisma.reply.findMany({
+            where: {
+                feedbackId,
+                rootId: null,
+            },
+            orderBy: { createTime: 'asc' },
+        });
+    }
 }
